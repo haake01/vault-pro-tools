d825c1270e9b15e48a47991279d0574e6f323de8
Kleber Haake <appsbrazil2@gmail.com>
2025-11-06 20:14:38 -0300

    Cria API index.js para deploy Vercel

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { auth } from "express-openid-connect";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  idpLogout: true,
+};
+
+app.use(auth(config));
+
+app.get("/", (req, res) => {
+  if (req.oidc.isAuthenticated()) {
+    res.send("✅ Logado com sucesso no Auth0!");
+  } else {
+    res.send('❌ Não logado — <a href="/login">Entrar</a>');
+  }
+});
+
+app.get("/profile", (req, res) => {
+  res.json(req.oidc.user);
+});
+
+export default app;
